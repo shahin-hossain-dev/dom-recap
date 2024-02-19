32350c545f9a814cdf8a857bd36e7bc1cf96b304
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-19 22:23:41 +0600

    dom recap

---
+++ recap.js
@@ -0,0 +1,8 @@
+/**
+ * 1. how to use js with html & CSS
+ * 2. get element or element form DOM
+ *      -> getElementById
+ *      -> getElementsByClassName
+ *      -> querySelector
+ *      -> querySelectorAll
+ */
